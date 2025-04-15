75be64319a1382b6286a6eff1c9f20d842322be5
Chavall315 <naufalbagaskara31@gmail.com>
2025-04-15 13:31:37 +0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,195 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, FlatList, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tailwind from 'twrnc'
+import  DateTimePicker  from '@react-native-community/datetimepicker'
+import { Try } from 'expo-router/build/views/Try'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { AntDesign, EvilIcons } from '@expo/vector-icons'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const index = () => {
 
-export default function HomeScreen() {
+  const [judul, setJudul] = useState('')
+  const [mapel, setMapel] = useState('')
+  const [deadline, setDeadLine] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [showDatePicker, setShowDatePicker] = useState(false)
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
+
+  const showPicker = () => {
+    setShowDatePicker(true);
+  }
+
+  const onDateChange = (event: any, selectedDate: any) => {
+    if (event.type === 'dismissed') {
+      setShowDatePicker(false);
+      return;
+    }
+
+    const currentDate = selectedDate || date;
+    setShowDatePicker(false);
+    setDate(currentDate);
+    setDeadLine(formatDate(currentDate));
+
+  }
+
+  
+  
+  const formatDate = (date) => {
+    const pad = (n) => n.toString().padStart(2, '0');
+    return `${pad(date.getDate())}/${pad(date.getMonth() + 1)}/${date.getFullYear()}`;
+  }
+
+  const addTask = () => {
+    if (judul === '' && mapel === '') return;
+    const newTask = {
+      id: Date.now().toString(),
+      judul: judul.trim(),
+      mapel: mapel.trim(),
+      deadline: deadline,
+      isChecked: false,
+  };
+    setList([...list, newTask]);
+    setJudul('');
+    setMapel('');
+    setDeadLine('');
+    setDate(new Date());
+    setShowDatePicker(false);
+    // Simpan newTask ke dalam state atau database sesuai kebutuhan
+  }
+
+  const saveTasks = async () => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(list));
+      console.log('Berhasil disimpan!');
+    } catch (error) {
+      console.error('Gagal menyimpan data:', error);
+    }
+  }
+
+  const loadTasks = async () => {
+    try {
+      const saved = await AsyncStorage.getItem('tasks');
+      if (saved !== null) {
+        setList(JSON.parse(saved));
+      }
+    } catch (error) {
+      console.error('Gagal memuat data:', error);
+    }
+  }
+
+  const deleteTask = (id: string) => {
+    const filtered = list.filter((item) => item.id !== id);
+    setList(filtered);
+  }
+
+  const handleEdit = () => {
+    const updated = list.map(item => item.id  === editId ? {...item, judul: judul , mapel: mapel , deadline: deadline } : item);
+    setList(updated)
+    setIsEditing(false);
+    setEditId(null);
+    setJudul('');
+    setMapel('');
+    setDeadLine('');
+  }
+
+  const startEdit = (item: any) => {
+    setJudul(item.judul);
+    setMapel(item.mapel);
+    setDeadLine(item.deadline);
+    setEditId(item.id);
+    setIsEditing(true);
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  useEffect(() => {
+    saveTasks();
+  }, [list]);
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style={tailwind`bg-blue-500 h-full`}>
+      <View style={tailwind`mb-10`}>
+        <Text style={tailwind`text-3xl font-bold mt-15 mx-10 mb-10`}>📚 TugasLu</Text>
+        <View style={tailwind`gap-3 bg-white shadow-2xl p-10 rounded-3xl mx-3`}>
+          {/* tempat form */}
+          <TextInput style={tailwind`bg-white border-2 border-gray-300 rounded-3xl p-5`}
+          placeholder='Masukan judul tugas'
+          value={judul}
+          onChangeText={setJudul} />
+          <TextInput style={tailwind`bg-white border-2 border-gray-300 rounded-3xl p-5`}
+          placeholder='Masukan Mata Pelajaran'
+          value={mapel}
+          onChangeText={setMapel} />
+          <View style={tailwind`flex-row items-center justify-between`}>
+            <TextInput style={tailwind`bg-white border-2 border-gray-300 rounded-3xl p-5 w-3/4`}
+            placeholder='Masukan Tanggal Deadline'
+            value={deadline}
+            editable={false} />
+            <TouchableOpacity style={tailwind`h-15 w-15 bg-white border-2 border-gray-300 rounded-3xl items-center justify-center`}
+            onPress={showPicker}>
+              <Text style={tailwind`text-center text-2xl font-bold`}>📅</Text>
+
+            </TouchableOpacity>
+          </View>
+          
+
+          <TouchableOpacity style={tailwind`bg-green-500 rounded-3xl p-5`}
+          onPress={isEditing ? handleEdit : addTask}>
+            <Text style={tailwind`text-white text-center text-xl font-bold`}>Masukan Tugas</Text>
+          </TouchableOpacity>
+
+          {showDatePicker && (
+            <DateTimePicker
+              value={date}
+              mode="date"
+              display="default"
+              onChange={onDateChange}
+              />
+          )}
+        </View>
+      </View>
+        <View style={tailwind`bg-white h-full rounded-3xl p-10 mx-5`}>
+          <ScrollView>
+            <FlatList
+            style={tailwind`mb-130`}
+            data={list}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+                <View style={tailwind`flex-row justify-between items-center mb-5 w-full p-5 rounded-3xl border border-gray-300 shadow bg-white`}>
+                <View style={tailwind`gap-3`}>
+                  <Text style={tailwind`font-bold text-2xl`}>{item.mapel}</Text>
+                  <Text style={tailwind``}>{item.judul}</Text>
+                  <Text style={tailwind``}>{item.deadline}</Text>
+                </View>
+                <View style={tailwind`items-center gap-3`}>
+                  <TouchableOpacity onPress={() => startEdit(item)}
+                    style={tailwind`bg-yellow-400 shadow-lg rounded-xl p-2 items-center justify-center w-15 h-15`}>
+                    <AntDesign name='edit' size={35} color='black'/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => {
+                    const updatedList = list.filter((task) => task.id !== item.id);
+                    setList(updatedList);
+                  }}
+                    style={tailwind`bg-red-400 shadow-lg rounded-xl p-2 items-center justify-center w-15 h-15`}>
+                    <EvilIcons name='trash' size={35} color='black'/>
+                  </TouchableOpacity>
+                </View>
+                
+              </View>
+              
+            )}
+            scrollEnabled={false}
+            />
+            </ScrollView>
+        </View>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default index
